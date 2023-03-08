544d17bca1846e0b1d17d0a04362b5dbd9fbefef
syj <syj@yxt.com>
2023-03-08 20:00:50 +0800

    feat(初始化版本): 1. 配置静态资源路径

---
+++ src/main/java/com/syj/prompt/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.syj.prompt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author syj@yxt.com
+ * @since 2023/3/8 19:52
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+}
